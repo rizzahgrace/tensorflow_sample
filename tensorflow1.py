9b7c6f00c3b7414b2bfe2a49b94a41d975442d8c
RIZZAH-PC\Rizzah <rizzahgrace@yahoo.com.ph>
2018-06-27 14:22:26 +0800

    Setup for Tensorflow

---
+++ tensorflow1.py
@@ -0,0 +1,34 @@
+from __future__ import absolute_import, division, print_function
+
+import os
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+import tensorflow.contrib.eager as tfe
+
+tf.enable_eager_execution()
+
+train_dataset_url = "http://download.tensorflow.org/data/iris_training.csv"
+
+train_dataset_fp = tf.keras.utils.get_file(fname=os.path.basename(train_dataset_url),
+                                           origin=train_dataset_url)
+
+def parse_csv(line):
+  example_defaults = [[0.], [0.], [0.], [0.], [0]]  # sets field types
+  parsed_line = tf.decode_csv(line, example_defaults)
+  # First 4 fields are features, combine into single tensor
+  features = tf.reshape(parsed_line[:-1], shape=(4,))
+  # Last field is the label
+  label = tf.reshape(parsed_line[-1], shape=())
+  return features, label
+
+train_dataset = tf.data.TextLineDataset(train_dataset_fp)
+train_dataset = train_dataset.skip(1)             # skip the first header row
+train_dataset = train_dataset.map(parse_csv)      # parse each row
+train_dataset = train_dataset.shuffle(buffer_size=1000)  # randomize
+train_dataset = train_dataset.batch(32)
+
+# View a single example entry from a batch
+features, label = iter(train_dataset).next()
+print("example features:", features[0])
+print("example label:", label[0])
